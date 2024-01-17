4602d58f5120420c57b4c8e245ffa9a26b714d39
DexyMaroni <divineinyang7@gmail.com>
2024-01-17 02:18:33 +0000

    Correction_3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux_user:0248"
+        "User-Agent": "linux_user"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
