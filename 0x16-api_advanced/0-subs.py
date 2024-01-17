3b6a5570218ceebbf40152c4ff9b555c721abed8
DexyMaroni <divineinyang7@gmail.com>
2024-01-17 01:59:30 +0000

    Correction_1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,33 +3,32 @@
 """ A function that queries the Reddit API and returns the mumber of subscribers for a given subreddit"""
 
 
-import requests 
+import requests
+
 
 def number_of_subscribers(subreddit):
-    """ Takes in an arg in a json format and sends a GET request to the Reddit API to 
-        get the number of subcribers 
-        
-        Parameter:
-            subreddit: A subcategory of reddit website.
+    """Takes in an arg in a json format and sends a GET request to the Reddit API to
+    get the number of subcribers
 
-        Returns:
-            The number of subcribers."""
+    Parameter:
+        subreddit: A subcategory of reddit website.
+
+    Returns:
+        The number of subcribers."""
     # Putting our url in a variable.
     address = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
 
-    
     # Creating a custom user_agent to avoid getting errors related to too many requests
-    headers = {
-        "Linux-2024.v1.0.0(user_1)"
-    }
+    headers = {"User-Agents" : "Linux-2024.v1.0.0(user_1)"}
 
     # allow_redirects = False because i dont want to control redirects manually.
     response = requests.get(address, headers=headers, allow_redirects=False)
 
-    # Initiating a control statement to help deal with error reponses
-    if reponse.status_code ==404:
+    # Initiating a control statement to help deal with error responses
+    if response.status_code == 404:
         return 0
 
-   # Converting our response from a json  format into a Python data and storing it in a variable.
+    # Converting our response from a json  format into a Python data and storing it in a variable.
     results = response.json().get("data")
     return results.get("subscribers")
+
